997863b5440e910d1f80c438541e8290b9ab3c93
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-28 18:01:21 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+  true
+else speed > 60 && speed < 40
+end
 end
 
 
 
 def not_safe?(speed)
-	
+  return (speed < 60 && speed > 40 ? false : true)
+  speed > 60 ? true : false
+  return true
+  speed < 40 ? true : false
+  return true
 end
-	
-
-
