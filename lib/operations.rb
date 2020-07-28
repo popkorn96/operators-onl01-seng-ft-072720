8e2eb35ee83fb9a4b95a7f300aacd820df580c0b
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-28 17:34:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,7 +14,7 @@ def not_safe?(speed)
   return true
   speed < 40 ? true : false
   return true
-  speed > 60 && speed > 40 ? false : true
+  speed < 60 && speed > 40 ? false : true
   return false
 end
 	
